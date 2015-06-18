0e92948a3f4a093d6cec92676c6bd14afed010a5
passion <huster.zhangpeng@gmail.com>
2015-06-18 12:05:10 +0800

    add install ubuntu shell

---
+++ shell/install-ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install vim wget curl nginx axel redis-server mongodb nodejs git ssh chromium-browser zsh
+sudo apt-get install android-tools-adb
+sudo apt-get install rbenv
+sudo apt-get install python-pip maven 
+sudo apt-get install dia
+sudo apt-get install libsdl2-dev
+sudo apt-get install vlc
+sudo apt-get install build-essential subversion checkinstall yasm
+sudo apt-get install valgrind
+sudo apt-get install ibus-googlepinyin
+
+sudo chsh -s /bin/zsh
+
+sudo pip install isort vim-debug
